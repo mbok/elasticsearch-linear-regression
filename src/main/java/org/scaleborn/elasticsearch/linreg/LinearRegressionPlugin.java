80cf6fa049594d41a693cf5dac8a3a90f1145e1b
mbok <mboook@gmail.com>
2017-03-05 23:49:45 +0100

    Gradle support

--- src/main/java/org/scaleborn/elasticsearch/linreg/LinearRegressionPlugin.java
+++ src/main/java/org/scaleborn/elasticsearch/linreg/LinearRegressionPlugin.java
@@ -1,8 +1,26 @@
 package org.scaleborn.elasticsearch.linreg;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.elasticsearch.plugins.Plugin;
 import org.elasticsearch.plugins.SearchPlugin;
+import org.elasticsearch.search.aggregations.metrics.stats.InternalStats;
+import org.elasticsearch.search.aggregations.metrics.stats.StatsAggregationBuilder;
 
 public class LinearRegressionPlugin extends Plugin implements SearchPlugin {
+
+	@Override
+	public List<AggregationSpec> getAggregations() {
+		AggregationSpec predict = new AggregationSpec(StatsAggregationBuilder.NAME, StatsAggregationBuilder::new, StatsAggregationBuilder::parse)
+        .addResultReader(InternalStats::new);
+		return Arrays.asList(predict);
+	}
+
+	@Override
+	public List<PipelineAggregationSpec> getPipelineAggregations() {
+		// TODO Auto-generated method stub
+		return SearchPlugin.super.getPipelineAggregations();
+	}
 
 }
