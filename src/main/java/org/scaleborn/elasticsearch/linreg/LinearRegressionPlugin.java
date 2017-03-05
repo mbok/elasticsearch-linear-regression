a379b16727e704ecf406536eefca40ce6f2b006e
mbok <mboook@gmail.com>
2017-03-05 22:29:26 +0100

    Initial commit

---
+++ src/main/java/org/scaleborn/elasticsearch/linreg/LinearRegressionPlugin.java
@@ -0,0 +1,8 @@
+package org.scaleborn.elasticsearch.linreg;
+
+import org.elasticsearch.plugins.Plugin;
+import org.elasticsearch.plugins.SearchPlugin;
+
+public class LinearRegressionPlugin extends Plugin implements SearchPlugin {
+
+}
