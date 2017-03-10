5cda751d7aa5d33f57b9c7ab2abab6f48471639c
mbok <mboook@gmail.com>
2017-03-10 22:35:15 +0100

    First try

--- src/main/java/org/scaleborn/elasticsearch/linreg/aggregation/predict/InternalPrediction.java
+++ src/main/java/org/scaleborn/elasticsearch/linreg/aggregation/predict/InternalPrediction.java
@@ -8,7 +8,6 @@ import org.elasticsearch.common.io.stream.StreamOutput;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.search.aggregations.InternalAggregation;
 import org.elasticsearch.search.aggregations.metrics.InternalNumericMetricsAggregation;
-import org.elasticsearch.search.aggregations.metrics.stats.Stats;
 import org.elasticsearch.search.aggregations.pipeline.PipelineAggregator;
 
 /**
@@ -17,39 +16,78 @@ import org.elasticsearch.search.aggregations.pipeline.PipelineAggregator;
 public class InternalPrediction extends InternalNumericMetricsAggregation.MultiValue implements
     Prediction {
 
+    private final long count;
 
-  public InternalPrediction(StreamInput in) throws IOException {
-    super(in);
-  }
+    public InternalPrediction(String name, long count,
+        List<PipelineAggregator> pipelineAggregators,
+        Map<String, Object> metaData) {
+        super(name, pipelineAggregators, metaData);
+        this.count = count;
+    }
 
-  @Override
-  public double value(String name) {
-    return 0;
-  }
+    public InternalPrediction(StreamInput in) throws IOException {
+        super(in);
+        count = in.readLong();
+    }
 
-  @Override
-  protected void doWriteTo(StreamOutput out) throws IOException {
+    @Override
+    public double value(String name) {
+        return 0;
+    }
 
-  }
+    @Override
+    protected void doWriteTo(StreamOutput out) throws IOException {
+        out.writeLong(count);
+    }
 
-  @Override
-  public InternalAggregation doReduce(List<InternalAggregation> aggregations,
-      ReduceContext reduceContext) {
-    return null;
-  }
+    @Override
+    public InternalAggregation doReduce(List<InternalAggregation> aggregations,
+        ReduceContext reduceContext) {
+        long count = 0;
+        for (InternalAggregation aggregation : aggregations) {
+            count += ((InternalPrediction) aggregation).count;
+        }
+        return new InternalPrediction(getName(), count, pipelineAggregators(), getMetaData());
+    }
 
-  @Override
-  public XContentBuilder doXContentBody(XContentBuilder builder, Params params) throws IOException {
-    return null;
-  }
+    @Override
+    public XContentBuilder doXContentBody(XContentBuilder builder, Params params)
+        throws IOException {
+        builder.field(Fields.PREDICTION, getPrediction());
+        return builder;
+    }
 
-  @Override
-  public String getWriteableName() {
-    return null;
-  }
+    @Override
+    public String getWriteableName() {
+        return PredictAggregationBuilder.NAME;
+    }
 
-  @Override
-  public double getPrediction() {
-    return 0;
-  }
+    @Override
+    public double getPrediction() {
+        return count / 2;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof InternalPrediction)) {
+            return false;
+        }
+
+        InternalPrediction that = (InternalPrediction) o;
+
+        return count == that.count;
+    }
+
+    @Override
+    public int hashCode() {
+        return (int) (count ^ (count >>> 32));
+    }
+
+    static class Fields {
+
+        public static final String PREDICTION = "prediction";
+    }
 }
---
+++ src/test/java/org/scaleborn/elasticsearch/linreg/aggregation/predict/InternalPredictionTests.java
@@ -0,0 +1,30 @@
+package org.scaleborn.elasticsearch.linreg.aggregation.predict;
+
+import java.util.List;
+import java.util.Map;
+import org.elasticsearch.common.io.stream.Writeable.Reader;
+import org.elasticsearch.search.aggregations.InternalAggregationTestCase;
+import org.elasticsearch.search.aggregations.pipeline.PipelineAggregator;
+
+/**
+ * Created by mbok on 09.03.17.
+ */
+public class InternalPredictionTests extends InternalAggregationTestCase<InternalPrediction> {
+
+    @Override
+    protected InternalPrediction createTestInstance(String name,
+        List<PipelineAggregator> pipelineAggregators, Map<String, Object> metaData) {
+        return new InternalPrediction(name, 0, pipelineAggregators,
+            metaData);
+    }
+
+    @Override
+    protected void assertReduced(InternalPrediction reduced, List<InternalPrediction> inputs) {
+
+    }
+
+    @Override
+    protected Reader<InternalPrediction> instanceReader() {
+        return InternalPrediction::new;
+    }
+}
