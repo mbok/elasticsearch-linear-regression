51434ad6b113143bf6787373b57b57463add8cc0
mbok <mboook@gmail.com>
2017-03-16 23:13:51 +0100

    Complete sampling logic with exact calculation of the covariance matrix

--- src/main/java/org/scaleborn/elasticsearch/linreg/sampling/SamplingData.java
+++ src/main/java/org/scaleborn/elasticsearch/linreg/sampling/SamplingData.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2017 Scaleborn UG, www.scaleborn.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.scaleborn.elasticsearch.linreg.sampling;
 
 /**
@@ -6,19 +22,17 @@ package org.scaleborn.elasticsearch.linreg.sampling;
  */
 public abstract class SamplingData {
 
-  private int featuresCount;
-  private long count = 0;
-  private double[] featureSums;
-  private double[] featureSquareSums;
-  private double[] featureTargetProductSum;
-  private double targetSum = 0;
-  private double targetSquareSum = 0;
+  protected int featuresCount;
+  protected long count = 0;
+  protected double[] featureSums;
+  protected double[] featureTargetProductSums;
+  protected double targetSum = 0;
+  protected double targetSquareSum = 0;
 
   public SamplingData(int featuresCount) {
     this.featuresCount = featuresCount;
     featureSums = new double[featuresCount];
-    featureSquareSums = new double[featuresCount];
-    featureTargetProductSum = new double[featuresCount];
+    featureTargetProductSums = new double[featuresCount];
   }
 
   public void sample(double[] featureValues, double targetValue) {
@@ -26,8 +40,7 @@ public abstract class SamplingData {
     for (int i = 0; i < featuresCount; i++) {
       double v = featureValues[i];
       featureSums[i] += v;
-      featureSquareSums[i] += v * v;
-      featureTargetProductSum[i] += v * targetValue;
+      featureTargetProductSums[i] += v * targetValue;
     }
     targetSum += targetValue;
     targetSquareSum += targetValue * targetValue;
@@ -36,21 +49,31 @@ public abstract class SamplingData {
 
   protected abstract void doSample(double[] featureValues, double targetValue);
 
-  public void merge(SamplingData from, SamplingData to) {
-    to.count += from.count;
-    for (int i = 0; i < from.featuresCount; i++) {
-      to.featureSums[i] += from.featureSums[i];
-      to.featureSquareSums[i] += from.featureSquareSums[i];
-      to.featureTargetProductSum[i] += from.featureTargetProductSum[i];
+  public void merge(SamplingData from) {
+    count += from.count;
+    for (int i = 0; i < featuresCount; i++) {
+      featureSums[i] += from.featureSums[i];
+      featureTargetProductSums[i] += from.featureTargetProductSums[i];
     }
-    to.targetSum += from.targetSum;
-    to.targetSquareSum += from.targetSquareSum;
-    doMerge(from, to);
+    targetSum += from.targetSum;
+    targetSquareSum += from.targetSquareSum;
+    doMerge(from);
   }
 
-  public abstract void doMerge(SamplingData from, SamplingData to);
+  public abstract void doMerge(SamplingData from);
 
   public abstract double[][] getCovarianceMatrix();
+
+  public double[] getAverages() {
+    double[] avgs = new double[featuresCount];
+    if (count > 0) {
+      for (int i = 0; i < featuresCount; i++) {
+        avgs[i] = featureSums[i] / count;
+      }
+    }
+    return avgs;
+  }
+
 
   public int getFeaturesCount() {
     return featuresCount;
@@ -64,12 +87,8 @@ public abstract class SamplingData {
     return featureSums;
   }
 
-  public double[] getFeatureSquareSums() {
-    return featureSquareSums;
-  }
-
-  public double[] getFeatureTargetProductSum() {
-    return featureTargetProductSum;
+  public double[] getFeatureTargetProductSums() {
+    return featureTargetProductSums;
   }
 
   public double getTargetSum() {
--- src/main/java/org/scaleborn/elasticsearch/linreg/sampling/exact/ExactSamplingData.java
+++ src/main/java/org/scaleborn/elasticsearch/linreg/sampling/exact/ExactSamplingData.java
@@ -1,32 +1,74 @@
+/*
+ * Copyright (c) 2017 Scaleborn UG, www.scaleborn.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.scaleborn.elasticsearch.linreg.sampling.exact;
 
 import org.scaleborn.elasticsearch.linreg.sampling.SamplingData;
 
 /**
- * Sampling data for a linear model solved exactly. Doing this that way has a
- * time complexity of O(C * C * N) and a memory consumption of O(C * C), where
- * C is the count of variables and N the count of observations / documents.
+ * Sampling data for a linear model where the covariance matrix is solved exactly. Doing this that
+ * way result in a time complexity of O(C² * N) and a memory consumption of O(C²), where C is
+ * the count of variables and N the count of observations / documents.
+ *
  * Created by mbok on 12.03.17.
  */
 public class ExactSamplingData extends SamplingData {
 
+  private double[][] featuresProductSums;
+
   public ExactSamplingData(int featuresCount) {
     super(featuresCount);
+    featuresProductSums = new double[featuresCount][];
+    for (int i = 0; i < featuresCount; i++) {
+      featuresProductSums[i] = new double[featuresCount];
+    }
   }
 
   @Override
   protected void doSample(double[] featureValues, double targetValue) {
-
+    for (int i = 0; i < featuresCount; i++) {
+      double vi = featureValues[i];
+      for (int j = 0; j < featuresCount; j++) {
+        featuresProductSums[i][j] += vi * featureValues[j];
+      }
+    }
   }
 
   @Override
-  public void doMerge(SamplingData from, SamplingData to) {
-
+  public void doMerge(SamplingData from) {
+    for (int i = 0; i < featuresCount; i++) {
+      for (int j = 0; j < featuresCount; j++) {
+        featuresProductSums[i][j] += ((ExactSamplingData) from).featuresProductSums[i][j];
+      }
+    }
   }
 
   @Override
   public double[][] getCovarianceMatrix() {
-    return new double[0][];
+    double[][] covMatrix = new double[featuresCount][];
+    double[] avgs = getAverages();
+    for (int i = 0; i < featuresCount; i++) {
+      double avgI = avgs[i];
+      covMatrix[i] = new double[featuresCount];
+      for (int j = 0; j < featuresCount; j++) {
+        double avgJ = avgs[j];
+        covMatrix[i][j] = featuresProductSums[i][j] - avgI * featureSums[j] - avgJ * featureSums[i]
+            + count * avgI * avgJ;
+      }
+    }
+    return covMatrix;
   }
-
 }
